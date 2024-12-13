0d444b34cbf0cc8ef892efa23669171af8c28150
Xavier <xavier.naidet@gmail.com>
2024-12-13 20:19:30 +0100

    Removed models folder from folow

--- src/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./projects";
-export * from "./socialMediats";
-export * from "./skills";
-export * from "./navigationLinks";
--- src/models/navigationLinks.ts
+++
@@ -1,6 +0,0 @@
-export const navigationLinks = [
-  { href: "/", label: "<Présentation>" },
-  { href: "/skills", label: "<Compétences>" },
-  { href: "/projects", label: "<Projets>" },
-  { href: "/contact", label: "<Contact>" },
-];
